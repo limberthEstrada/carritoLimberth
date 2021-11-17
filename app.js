55d995c443ece009386af03aefe799c5f5ecea67
limberthEstrada <lvasquezestrada@frba.utn.edu.ar>
2021-11-17 19:47:47 -0300

    agrego mi parte

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+app.use(express.static('public'));
+ 
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./views/carrito.html'))
+})
