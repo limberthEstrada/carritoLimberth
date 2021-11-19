dae4fa5dbbd22f1ff81955da6e0b678892138951
limberthEstrada <lvasquezestrada@frba.utn.edu.ar>
2021-11-19 10:47:35 -0300

    aver

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-
+const path = require('path')
 
 app.listen (process.env.PORT ||3000, ()=>{
     console.log('Servidor funcionando bien');
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + './views/index.html');
+    res.sendFile(path.resolve('./views/index.html'));
 });
 
 app.get('/carrito', (req,res)=>{
-    res.sendFile(__dirname + './views/carrito.html');
+    res.sendFile(path.resolve('./views/carrito.html'));
 });
 
