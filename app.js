41cfd3758edcfe77922de9eb91660b8abc6dc260
limberthEstrada <lvasquezestrada@frba.utn.edu.ar>
2021-11-19 09:33:03 -0300

    xdxxd

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const app = express();
-const path = require('path')
 app.use(express.static('public'));
-
 
 
 app.listen (process.env.PORT ||3000, ()=>{
     console.log('Servidor funcionando bien');
-
 });
 
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + './views/index.html');
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve('./views/index.html'))
-})
+app.get('/carrito', (req,res)=>{
+    res.sendFile(__dirname + './views/carrito.html');
+});
 
-app.get('/carrito', (req, res) => {
-    res.sendFile(path.resolve('./views/carrito.html'))
-})
